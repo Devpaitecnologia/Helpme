abd3751d7be4958bb3920ebb7a265f076460d86f
Devpaitecnologia <januariobernardo602@gmail.com>
2026-03-29 15:57:16 +0200

    Fix sender value assignment in sendMsg function
    Signed-off-by: Devpaitecnologia <januariobernardo602@gmail.com>

--- ele.js
+++ ele.js
@@ -35,8 +35,8 @@ var sender = document.getElementById('nome');
             var lc = locali.value;
             var mn = msgn.value;
             var md = msgd.value;
+         var mdi = sender.value;
          var mdi = msgdi.value;
-         
             var timestamp = new Date().getTime();
             set(ref(db,"messag/"+timestamp),{
                 lc : lc,
